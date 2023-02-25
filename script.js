ea2a4a68b8e1b404889a113c38714c53ef472bdc
sanjay kumar <70475208+snjy-kumar@users.noreply.github.com>
2023-02-25 21:47:22 +0000

    some_changes

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-//await = makes an async function wait for a promise
-async function loadFile(){
-  
-  let fileLoaded = false;
+// The idea behind a module is that it's file of reusable
+// We can import sections of pre-written code to use whenever
+// Great for any general utility values and functions
+// Helps to make your code more reusable and maintanalble
+// Think of modules as seperate chapter of a book
 
-  if(fileLoaded){
-    return "File Loaded";
-  }
-  else{
-    throw "File NOT Loaded";
-  }
-};
 
-async function startProcess(){
-  try{
-  let message = await loadFile();
-  console.log(message);
-    
-  }
-  catch(error){
-    console.log(error);
-  }
-}
+import { PI, getCircumference, getArea } from "./math";
 
-startProcess();
+alert(PI);
+
+let circumference = getCircumference(10);
+console.log(circumference);
+
+let area = getArea(5);
+console.log(area);
